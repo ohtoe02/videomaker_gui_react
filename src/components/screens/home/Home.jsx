4c18b5a2b26d0be1b113a6bcc4cc630abba40b97
ohehh <ohtoe02@gmail.com>
2022-07-15 13:55:26 +0500

    Validation improved

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 import QuestionMark from "../../ui/icons/question_mark/QuestionMark";
 import {
     Autocomplete,
-    Button, Checkbox,
+    Button, Checkbox, CircularProgress,
     Divider,
     FormControl, FormControlLabel,
     InputLabel, MenuItem, Select,
@@ -16,23 +16,11 @@ import {tts_engines} from "../../../utils/constants";
 
 const Home = () => {
     const [currentVoices, setCurrentVoices] = useState([])
+    const [loading, setLoading] = useState(true)
     const [startupConfig, setStartupConfig] = useState({})
     const [tts_engine, setTtsEngine] = useState('tiktok')
     const [currentVoice, setCurrentVoice] = useState('Stitch')
-    const [inputs, setInputs] = useState({
-        username: '',
-        password: '',
-        client_id: '',
-        client_secret: '',
-        subreddit: '',
-        subreddit_post_id: '',
-        comment_length: '',
-        times_to_run: '',
-        comment_opacity: '',
-        theme: 'light',
-        allow_nsfw: false,
-        reddit_2fa: false
-    })
+    const [theme, setTheme] = useState('light')
 
     const voice_variable_names = {
         'streamlabs': 'streamlabs_voice',
@@ -41,14 +29,18 @@ const Home = () => {
         'aws': 'aws_voice',
     }
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
     useEffect(() => {
         const makeRequest = async () => {
-            const response = await fetch('/backend/')
+            const response = await fetch('http://localhost:8000/backend/')
             const config = await response.json()
+            config['allow_nsfw'] = config['allow_nsfw'] === 'True'
+            setTheme(config.theme)
             setStartupConfig(config)
+            setLoading(false)
         }
+
         makeRequest()
     }, [])
 
@@ -59,26 +51,27 @@ const Home = () => {
         setCurrentVoices(currentVoices)
     }, [ tts_engine ])
 
-    const handleInputUpdate = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        setInputs(values => ({...values, [name]: value}))
+    const handleThemeUpdate = (e) => {
+        setTheme(e.target.value)
     }
 
     const handleTTSEngineUpdate = (e) => {
         setTtsEngine(e.target.value)
-        setCurrentVoice('')
+        const voices = tts_engines.find(item => item._id === e.target.value).voices
+        const currentVoices = Object.keys(voices)
+        setCurrentVoice(currentVoices[0])
     }
 
     const handleVoiceChange = (e, value) => {
-        setCurrentVoice(value)
+        if (value)
+            setCurrentVoice(value)
     }
 
-    const handleCheckChange = (e) => {
-        const name = e.target.name;
-        const value = e.target.checked;
-        setInputs(values => ({...values, [name]: value}))
-    }
+    // const handleCheckChange = (e) => {
+    //     const name = e.target.name;
+    //     const value = e.target.checked;
+    //     setInputs(values => ({...values, [name]: value}))
+    // }
 
     const submitHandler = async (data) => {
         // const payload_template = {
@@ -106,11 +99,12 @@ const Home = () => {
             ...data,
             aws_voice: '',
             postlang: '',
+            theme: theme,
             tiktok_voice: '',
             streamlabs_voice: '',
             opacity: parseFloat(data.opacity),
             max_comment_length: parseInt(data.max_comment_length),
-            reddit_2fa: inputs.reddit_2fa ? 'yes' : 'no'}
+            reddit_2fa: data.reddit_2fa ? 'yes' : 'no'}
 
         const voice_id = tts_engines.find(engine => engine._id === tts_engine).voices[currentVoice]._id
 
@@ -118,7 +112,7 @@ const Home = () => {
 
         // axios.get()
 
-        const item = await fetch('/backend/update', {
+        const item = await fetch('http://localhost:8000/backend/update', {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
@@ -137,6 +131,8 @@ const Home = () => {
     <div style={ {padding: '16px 0'} }>
         <h1 className={styles.header}><span style={{fontSize: '48px'} }>🎥</span> Reddit Video Maker <span style={{fontSize: '48px'} }>🎥</span></h1>
         <section className={styles.wrapper}>
+            {loading ? <CircularProgress /> :
+
             <form
               className={styles.form}
               // onSubmit={e => submitHandler(e)}
@@ -144,85 +140,108 @@ const Home = () => {
             >
                 <section className={styles['input-section']}>
                     <TextField
-                      required
+                      error={errors.reddit_username}
                       name={'username'}
                       id="username-input"
                       label="Reddit username"
                       sx={inputStyleCorrection}
-                      defaultValue={startupConfig.reddit_username}
-                      {...register('reddit_username', { minLength: 3, maxLength: 20, required: true })}
+                      defaultValue={startupConfig['reddit_username']}
+                      helperText={ errors.reddit_username ? errors.reddit_username?.message : ' ' }
+                      {...register('reddit_username', { minLength: {value: 3, message: 'Reddit username length must be between 3 and 20 symbols'}, maxLength: {value: 20, message: 'Reddit username length must be between 3 and 20 symbols'}, required: {value: true, message: 'Reddit username is required'} })}
                     />
                     <TextField
-                      required
+                      error={errors.reddit_password}
                       name={'password'}
                       id="password-input"
                       label="Reddit password"
                       type="password"
                       sx={inputStyleCorrection}
-                      {...register('reddit_password', { minLength: 8, maxLength: 30, required: true })}
+                      defaultValue={startupConfig['reddit_password']}
+                      helperText={ errors.reddit_password ? errors.reddit_password?.message : ' ' }
+                      {...register('reddit_password', { minLength: {value: 8, message: 'Reddit password length must be between 8 and 30 symbols'}, maxLength: {value: 30, message: 'Reddit password length must be between 8 and 30 symbols'}, required: {value: true, message: 'Reddit password is required'} })}
                     />
                     <TextField
-                      required
+                      error={errors.reddit_client_id}
                       name={'client_id'}
                       id="client-id-input"
                       label="Reddit client ID"
                       sx={inputStyleCorrection}
-                      {...register('reddit_client_id', { minLength: 12, maxLength: 30, required: true })}
+                      defaultValue={startupConfig['reddit_client_id']}
+                      helperText={ errors.reddit_client_id ? errors.reddit_client_id?.message : ' ' }
+                      {...register('reddit_client_id', { minLength: {value: 12, message: 'Reddit client ID length must be between 12 and 30 symbols'}, maxLength: {value: 30, message: 'Reddit client ID length must be between 12 and 30 symbols'}, required: {value: true, message: 'Reddit client ID is required'} })}
                     />
                     <TextField
-                      required
+                      error={errors.reddit_client_secret}
                       name={'client_secret'}
                       id="client-secret-input"
                       label="Reddit client secret"
                       sx={inputStyleCorrection}
-                      {...register('reddit_client_secret', { minLength: 20, maxLength: 40, required: true })}
+                      defaultValue={startupConfig['reddit_client_secret']}
+                      helperText={ errors.reddit_client_secret ? errors.reddit_client_secret?.message : ' ' }
+                      {...register('reddit_client_secret', { minLength: {value: 20, message: 'Reddit client secret length must be between 20 and 40 symbols'}, maxLength: {value: 40, message: 'Reddit client secret length must be between 20 and 40 symbols'}, required: {value: true, message: 'Reddit client secret is required'} })}
                     />
                     <TextField
-                      required
+                      error={errors.subreddit}
                       name={'subreddit'}
                       id="subreddit-input"
                       label="Subreddit"
                       placeholder={"r/subreddit"}
                       className={styles.wide}
                       sx={inputStyleCorrection}
-                      {...register('subreddit', { required: 'Subreddit is required' })}
+                      defaultValue={startupConfig['subreddit']}
+                      helperText={ errors.subreddit ? errors.subreddit?.message : ' ' }
+                      {...register('subreddit', { minLength: {value: 5, message: "Subreddit name length must be bigger than 5 symbols"}, required: {value: true, message: 'Subreddit is required'}, pattern: {value: /r\/[A-Za-z]/i, message: 'Subreddit must be r/subreddit'} })}
                     />
                     <Divider className={styles.wide}/>
                     <TextField
+                      error={errors.post_id}
                       name={'subreddit_post_id'}
                       id="post-id-input"
+                      multiline
                       label="Subreddit post ID"
-                      inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
                       sx={inputStyleCorrection}
-                      {...register('post_id', { required: true })}
+                      defaultValue={startupConfig['post_id']}
+                      helperText={ ' ' }
+                      {...register('post_id')}
                     />
                     <TextField
+                      error={errors.times_to_run}
                       name={'times_to_run'}
                       id="times-to-run-input"
                       label="Times to run"
                       placeholder={'Default - 1'}
                       sx={inputStyleCorrection}
-                      {...register('times_to_run', {min: 1, max: 100, required: true })}
+                      defaultValue={startupConfig['times_to_run']}
+                      helperText={ errors.times_to_run ? errors.times_to_run?.message : ' ' }
+                      {...register('times_to_run', {min: {value: 1, message: 'Times to run value must be between 1 and 100'}, max: {value: 100, message: 'Times to run value must be between 1 and 100'}, required: {value: true, message: 'Times to run is required'} })}
                     />
-                    <TextField
-                      name={'comment_length'}
-                      id="max-comment-length-input"
-                      label="Max comment length"
-                      placeholder={'Default - 500'}
-                      sx={inputStyleCorrection}
-                      {...register('max_comment_length', { min: 0, max: 10000, required: true })}
-                    />
+                    <div style={{display: 'flex', flexDirection: 'column'}}>
+                        <TextField
+                          error={errors.max_comment_length}
+                          name={'comment_length'}
+                          id="max-comment-length-input"
+                          label="Max comment length"
+                          placeholder={'Default - 500'}
+                          sx={inputStyleCorrection}
+                          defaultValue={startupConfig['max_comment_length']}
+                          helperText={ errors.max_comment_length ? errors.max_comment_length?.message : ' ' }
+                          {...register('max_comment_length', { min: {value: 0, message: 'Max comment length value must be between 0 and 10000'}, max: {value: 10000, message: 'Max comment length value must be between 0 and 10000'}, required: {value: true, message: 'Max comment length is required'} })}
+                        />
+                    </div>
                     <div style={{display: "flex", alignItems: 'center', gap: '8px'}}>
                         <TextField
+                          error={errors.opacity}
                           name={'comment_opacity'}
                           id="opacity-input"
                           label="Comment opacity"
                           placeholder={'0.0 - 1.0'}
                           fullWidth
                           sx={inputStyleCorrection}
-                          {...register('opacity', { min: 0, max: 1, required: true })}
+                          defaultValue={startupConfig['opacity']}
+                          helperText={ errors.opacity ? errors.opacity?.message : ' ' }
+                          {...register('opacity', { min: {value: 0, message: 'Opacity value must be between 0 and 1'}, max: {value: 1, message: 'Opacity value must be between 0 and 1'}, required: {value: true, message: 'Opacity is required'} })}
                         />
-                        <QuestionMark />
+                        {/*<QuestionMark />*/}
                     </div>
                     <FormControl fullWidth sx={inputStyleCorrection}>
                         <InputLabel id="tts-select-label">TTS Engine</InputLabel>
@@ -253,10 +272,10 @@ const Home = () => {
                     <div style={{display: 'flex', gap: '24px', alignItems: 'center'}}>
                         <ToggleButtonGroup
                           size={"large"}
-                          value={inputs.theme}
+                          value={theme}
                           exclusive
-                          onChange={e => handleInputUpdate(e)}
-                          aria-label="text alignment"
+                          onChange={e => handleThemeUpdate(e)}
+                          aria-label="Screenshots theme"
                           sx={{ fontFamily: "Inter, serif" }}
                         >
                             <ToggleButton name={'theme'} value='light'>Light</ToggleButton>
@@ -268,7 +287,9 @@ const Home = () => {
                           control={<Checkbox
                             name={'allow_nsfw'}
                             {...register('allow_nsfw')}
-                            sx={{display: 'flex', marginLeft: '8px', height: '42px'}} />}
+                            defaultChecked={startupConfig.allow_nsfw}
+                            sx={{display: 'flex', marginLeft: '8px', height: '42px'}}
+                          />}
                           label={'Allow NSFW'}
                         />
                         <FormControlLabel
@@ -276,6 +297,7 @@ const Home = () => {
                           control={<Checkbox
                             name={'reddit_2fa'}
                             {...register('reddit_2fa')}
+                            defaultChecked={startupConfig.reddit_2fa === 'yes'}
                             sx={{display: 'flex', height: '42px'}} />}
                           label={'2FA'}
                         />
@@ -289,6 +311,7 @@ const Home = () => {
                     Accept
                 </Button>
             </form>
+            }
         </section>
     </div>
     )
